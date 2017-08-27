1a9200a2cdb8290f335f362fdfd0a27b32a21721
Abhinav Dinesh C <abhinavdinesh95@gmail.com>
2017-08-27 12:05:41 +0530

    Added comments

--- RemindMe.py
+++ RemindMe.py
@@ -3,8 +3,6 @@ import subprocess
 import mechanize
 from urllib2 import HTTPError
 from twilio.rest import Client
-#from plyer import notification
-# import BeautifulSoup
 
 # Your Account SID from twilio.com/console
 account_sid = "<replace_account_sid>"
@@ -14,8 +12,13 @@ auth_token  = "<replace_auth_token>"
 client = Client(account_sid, auth_token)
 
 br = mechanize.Browser()
+# Open website to check availability
 request = br.open("http://www.indianrail.gov.in/acc_avl.html")
 
+# Set avaiability threshold limit 
+threshold = 50
+
+# List out day, month, train number, source and destination of trains
 trains = [["15","09","16629","KZK","CLT"],["22","09","16629","KZK","CLT"]]
 
 i = 0
@@ -27,28 +30,41 @@ while i < len(trains) :
     br["lccp_trnno"] = trains[i][2]
     br["lccp_srccode"] = trains[i][3]
     br["lccp_dstncode"] = trains[i][4]
+    # Set class as Sleeper
     br["lccp_class1"] = ["SL"]
+    # Set quota as General
     br["lccp_quota"] = ["GN"]
+    
     try:
+        # Submit form
         response = br.submit()
         res = response.read()
+        
+        #Save response source code in file for debugging
         f = open('html_response_code','w')
         f.write(res)
         f.close()
+        
+        #Pattern matching on response from br.submit()
         pattern = ["AVAILABLE \d+"]
         match = re.search(pattern[0],res)
         statustext = res[match.start():match.end()]
         currentavl = int(statustext.split()[1])
         print(currentavl)
         br.back()
-        if currentavl < 160 :
-            # message = client.messages.create(
-            # to="+919495206270", 
-            # from_="+13853932702",
-            # body="Train:%s  from  %s-%s on  %s-%s-2017 has only %s seats left." % (train[2],train[3],train[4],train[0],train[1],currentavl))
+        
+        #Check if currrent availability less than threshold set
+        if currentavl < threshold :
+            message = client.messages.create(
+            to="<your_phone_number>", 
+            from_="<your_twilio_number>",
+            body="Train:%s  from  %s-%s on  %s-%s-2017 has only %s seats left." % (train[2],train[3],train[4],train[0],train[1],currentavl))
             print ("Alert Send")
+        
+        # Increment i for next iteration
         i +=1
     except HTTPError:
+        #Catch HTTP Error and redo the current iteration
         print ("404 Error")
         request = br.open("http://www.indianrail.gov.in/acc_avl.html")
         continue
